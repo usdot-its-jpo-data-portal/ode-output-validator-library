c6c6e1185eebb3b025f7ce2dd6f9f0eb6df54e4d
chuehlien <chuehlien@gmail.com>
2020-04-15 17:22:19 -0400

    modify setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ setup(
     version="0.0.5",
     description="ODE Data Validation Library",
     packages=find_packages(),
-    package_data={'odevalidator': ['configs/config.ini']},
+    package_data={'odevalidator': ['configs/']},
     include_package_data=True,
     test_suite="tests",
+    install_requires=[
+       'pytz',
+       'python-dateutil'
+    ]
 )
